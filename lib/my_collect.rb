6862690c8f1b8269823d00b3d0c9f52a61f850e4
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 02:39:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
-def my_collect(languages)
+def my_collect(array)
 empty_array = []
 counter = 0
 
-while my_collect.collect do 
+while my_collect.collect do
   my_collect.split(" ")
   yield my_collect
   counter += 1
