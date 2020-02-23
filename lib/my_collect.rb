6c6fa2a50aaf6a6bff0a64aca71c9231a8058c56
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 03:43:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
 empty_array = []
 counter = 0
 
-while counter.upcase < array.length  do
-yield array[counter.upcase]
+while counter < array.length  do
+yield array[counter]
 counter += 1
 
 end
