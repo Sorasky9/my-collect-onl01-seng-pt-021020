98ab34fa1cd6ed2dac5e44bc508c1e77e47ab3f0
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 03:50:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
 empty_array = []
 counter = 0
 
-while counter < array.length 
-empty_array << yield array[counter]
+while counter < array.length
+empty_array << yield array
 counter += 1
 
 end
